61702f3f4df26c3efab69e3d3331ff37687f9bbb
Vladislav Geyer <vladislav.geyer@yandex.ru>
2020-07-01 18:08:41 +0300

    Добавлены логгеры

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const { PORT = 3000 } = process.env;
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
+const { requestLogger, errorLogger } = require('./middlewars/loggers');
 const user = require('./routes/users');
 const article = require('./routes/articles');
 const pageNotExist = require('./middlewars/page-not-exist');
@@ -31,9 +32,11 @@ async function getStarted(next) {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(requestLogger);
 app.use(user);
 app.use(article);
 app.use(pageNotExist);
+app.use(errorLogger);
 app.use(errors());
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
